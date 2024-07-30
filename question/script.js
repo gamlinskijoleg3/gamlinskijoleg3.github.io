b0cf8016b47e7a807d93aa7788d6840822b19cf7
gamlinskijoleg <gamlinskijoleg@gmail.com>
2024-07-30 21:54:47 +0300

    x

--- question/script.js
+++ question/script.js
@@ -1,16 +1,34 @@
-const question1Answer = "Христофор Колумб";
+// cкорочення
 const answer1 = document.getElementById("answer1");
 const answer2 = document.getElementById("answer2");
 const answer3 = document.getElementById("answer3");
 const answer4 = document.getElementById("answer4");
+const nextQuestionButton = document.getElementById("next-question");
+const previousQuestionButton = document.getElementById("prevoius-question");
+const currentQuestion = document.getElementById("current-question-number");
+const question = document.getElementById("question-itself");
+const submitButton = document.getElementById("submit-button");
+const goto = document.getElementById("goto");
 
+// стани
 var answerClicked = "";
-var correctAnswers = new Array("Христофор Колумб");
-var questions = new Array("Хто відкрив Америку?");
-var answer1variants = new Array("Марко Поло");
-var answer2variants = new Array("Марко Поло");
-var answer3variants = new Array("Марко Поло");
-var answer4variants = new Array("Марко Поло");
+
+// масиви
+var correctAnswers = new Array("Христофор Колумб",);
+var questions = new Array("Хто відкрив Америку?", "wsws");
+var answer1variants = new Array("Марко Поло", "jgfds");
+var answer2variants = new Array("Васко да Гама", "dfejk");
+var answer3variants = new Array("Фернан Магеллан", " rghjkl;lku tgfghjk");
+var answer4variants = new Array("Христофор Колумб", "poil;kj");
+
+function adder(){
+    index = Number(currentQuestion.innerHTML) -1; 
+    question.innerHTML = questions[index];
+    answer1.innerHTML = answer1variants[index];
+    answer2.innerHTML = answer2variants[index];
+    answer3.innerHTML = answer3variants[index];
+    answer4.innerHTML = answer4variants[index];
+}
 
 answer1.onclick = function () {
   answerClicked = answer1.innerHTML;
@@ -40,96 +58,26 @@ function checkAnswer() {
   }
 }
 
-const nextQuestionButton = document.getElementById("next-question");
-const currentQuestion = document.getElementById("current-question-number");
-
 nextQuestionButton.onclick = function () {
   if (currentQuestion.innerHTML < 20) {
     currentQuestion.innerHTML = Number(currentQuestion.innerHTML) + 1;
-    questionChecker();
-  } else {
-    console.log("No questions left!");
+    adder()
   }
 };
 
-const question = document.getElementById("question-itself");
-
-function questionChecker() {
-  switch (Number(currentQuestion.innerHTML)) {
-    case 1:
-      question.innerHTML = questions[0];
-      break;
-
-    case 2:
-      question.innerHTML = questions[1];
-      break;
-
-    case 3:
-      question.innerHTML = questions[2];
-      break;
-
-    case 4:
-      question.innerHTML = questions[3];
-      break;
-    case 5:
-      break;
-
-    case 6:
-      break;
-
-    case 7:
-      break;
-
-    case 8:
-      break;
-
-    case 9:
-      break;
-
-    case 10:
-      break;
-
-    case 11:
-      break;
-
-    case 12:
-      break;
-
-    case 13:
-      break;
-
-    case 14:
-      break;
-
-    case 15:
-      break;
-
-    case 16:
-      break;
-
-    case 17:
-      break;
-
-    case 18:
-      break;
-
-    case 19:
-      break;
-
-    case 20:
-      question.innerHTML = questions[0];
-      break;
-  }
-}
-const submitButton = document.getElementById("submit-button");
-const goto = document.getElementById("goto");
+previousQuestionButton.onclick = function () {
+    if (currentQuestion.innerHTML >1 ) {
+      currentQuestion.innerHTML = Number(currentQuestion.innerHTML) - 1;
+      adder()
+    }
+  };
 
 submitButton.onclick = function () {
   var currentQuestionEntered = goto.value;
   currentQuestionEntered = Math.ceil(currentQuestionEntered);
   if (currentQuestionEntered <= 20 && currentQuestionEntered > 0) {
     currentQuestion.innerHTML = currentQuestionEntered;
-    questionChecker();
+    adder()
   } else {
     console.log("You pidaras");
   }
